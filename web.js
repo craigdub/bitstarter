f322409e76510b67dd82a61bc5bfea3711d00116
Ubuntu <ubuntu@ip-172-31-18-117.us-west-2.compute.internal>
2013-07-11 00:10:42 +0000

    fixed thing

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buf = fs.readSyncFile('index.html')
+	var buf = fs.readFileSync('index.html')
   response.send(buf.toString());
 });
 
